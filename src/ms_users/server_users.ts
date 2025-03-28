6991af63676c22fcf66d086c9906d8a45f4b8d29
MentalSmoge <mentalsmoge@gmail.com>
2025-03-28 18:46:34 +0500

    sas16

--- src/ms_users/server_users.ts
+++ src/ms_users/server_users.ts
@@ -12,7 +12,7 @@ const port = process.env.USERS_PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
-//sas15
+//sas16
 async function startUsersServer() {
 	// Инициализация PostgreSQL
 	const pool = createPool();
